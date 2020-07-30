1e274a217d1899c1ab4dc0cbd1bf02e3ded4d538
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-30 14:00:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,7 +11,7 @@ def join_nested_strings(src)
         else
           new_array.push(src[row_index][element_index])
         end
-
+        element_index += 1
       end
       row_index += 1
     end
