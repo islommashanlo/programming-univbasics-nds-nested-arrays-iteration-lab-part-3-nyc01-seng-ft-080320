368f595d4ae85ea45a9a21880c1b6eb6dc7e3e17
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-30 13:59:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
       element_index = 0
       while element_index < src[row_index].length do
         if src[row_index][element_index].is_a?(Integer)
-          then
+          then src[row_index].shift(element_index)
       end
       row_index += 1
     end
